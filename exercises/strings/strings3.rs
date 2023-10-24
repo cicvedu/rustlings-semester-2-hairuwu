5a7bcd45488bd0fa7f9bddda7ba5cfe7c24c1e00
Hairu Wu <15655678236@163.com>
2023-10-24 04:01:04 +0000

    feat: string3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,19 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    return &String::trim_a_string(str);
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s2 = " world!";
+    input += s2;
+    return input;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    return input.replace("cars", "balloons");
 }
 
 #[cfg(test)]
