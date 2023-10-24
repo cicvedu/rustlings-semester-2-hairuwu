04371996debcddf5732b283bca210e55aeffe295
Hairu Wu <15655678236@163.com>
2023-10-24 04:25:08 +0000

    feat: update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,18 +3,20 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    return &String::trim_a_string(str);
+    let s1 = String::from(input);
+    let output = s1.trim();
+    return output.to_string();
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
+    let s1 = String::from(input);
     let mut s2 = " world!";
-    input += s2;
-    return input;
+    let output = s1 + s2;
+    return output;
 }
 
 fn replace_me(input: &str) -> String {
