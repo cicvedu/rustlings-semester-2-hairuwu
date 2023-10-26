c5626ce11813aedd7a6501dc1732292f360bac08
Hairu Wu <15655678236@163.com>
2023-10-26 12:18:40 +0000

    feat: update

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 struct Point {
     x: i32,
@@ -13,9 +13,9 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => panic!("no match!"),
+        _ => println!("no match!"),
     }
     y; // Fix without deleting this line.
 }
